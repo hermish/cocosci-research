77c9cfcd8c9e4f862da3099dfd81e0aa514bbf7b
Hermish Mehta <hermish@berkeley.edu>
2017-10-02 00:41:57 -0700

    Complete scraper that will be used

--- scraping/extractors.py
+++ scraping/extractors.py
@@ -1,38 +1,69 @@
+import time
+
 TEST_SUB = 'explainlikeimfive'
-SUBREDDITS = ['explainlikeimfive']
+API_LIMIT = 30
+SLEEP_TIME = 60
 
 
-def map_func(func, dictionary):
+def map_to_values(func, dictionary):
     """
-    Maps a function to the values of a dictionary without mutation
     :param func: (function) function to be mapped
     :param dictionary: (dict) the original hash map
-    :return: (dict) new dictionary with different values
+    :return: (dict) new dictionary with different values,
+        mapping the function to the values of the dictionary
     """
     return {key: func(value) for key, value in dictionary.items()}
 
 
+def respect_limit(requests):
+    """
+    :param requests: (int) the current number of
+    :return: (int) an updated number of requests, sleeping the required amount of
+        time and resetting if an additional call would push the requests over the
+        specified limit
+    """
+    if requests + 1 > API_LIMIT:
+        time.sleep(SLEEP_TIME)
+        return 0
+    return requests + 1
+
+
+def get_posts(reddit, subreddits, mode, time_filter, num_comments):
+    """
+    :param reddit: (reddit) an possibly unauthenticated reddit instance
+    :param subreddits: [str] the subreddits to pull data from
+    :param mode: (str) the string referring to browsing mode, for example 'rising'
+    :param time_filter: (str) the time period for the mode selected
+    :param num_comments: (int) the
+    :return: (gen) a generator which returns, at every successive call, list of each
+        post and the requested number of top comments until exhaustion
+    """
+    requests = 0
+    for sub in subreddits:
+        subreddit = reddit.subreddit(sub)
+        post_gen = getattr(subreddit, mode)(time_filter=time_filter)
+        requests = respect_limit(requests)
+        for submission in post_gen:
+            requests = respect_limit(requests)
+            output = [map_to_values(str, vars(submission))]
+            comments_left = num_comments
+            requests = respect_limit(requests)
+            for comment in submission.comments:
+                requests = respect_limit(requests)
+                if not comments_left:
+                    break
+                output.append(map_to_values(str, vars(comment)))
+                comments_left -= 1
+            yield output
+
+
 def test_extractor(reddit):
     """
-    Outputs 2 submission each with at most 3 comments each from the test subreddit defined
-    :param reddit: (reddit) an unauthenticated reddit instance
-    :return: (str) yields information to be written
+    :param reddit: (reddit) a possibly unauthenticated reddit instance
+    :return: (str) yields 1 submission followed by all its comments
     """
     subreddit = reddit.subreddit(TEST_SUB)
-    top_gen = subreddit.top(time_filter='all', limit=2)
-    for submission in top_gen:
-        everything, num = vars(submission), 0
-        yield map_func(str, everything)
+    for submission in subreddit.top(time_filter='all', limit=1):
+        yield map_to_values(str, vars(submission))
         for comment in submission.comments:
-            if num > 2: break
-            yield map_func(str, vars(comment))
-            num += 1
-
-
-def get_explanations(reddit):
-    """
-    Extract desired information from comments
-    :param reddit: (reddit) an unauthenticated reddit instance
-    :return:
-    """
-    pass
+            yield map_to_values(str, vars(comment))
--- scraping/main.py
+++ scraping/main.py
@@ -1,10 +1,14 @@
 from utilities import read_credentials, scrape_data, write_file
-from extractors import test_extractor
+from extractors import test_extractor, get_posts
 
 
 FILE_NAME = 'credentials.txt'
 OUTPUT = 'output.txt'
-EXTRACTOR = test_extractor
+OPTIONS = {'subreddits': ['explainlikeimfive'],
+           'mode': 'top',
+           'time_filter': 'week',
+           'num_comments': 3}
+EXTRACTOR = lambda reddit: get_posts(reddit, **OPTIONS)
 
 credentials = read_credentials(FILE_NAME)
 result = scrape_data(*credentials, EXTRACTOR)
