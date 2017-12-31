4dd2f39ddc088c32332bfb41b8a44e1e8b1283c9
Hermish Mehta <hermish@berkeley.edu>
2017-12-31 06:41:09 -0800

    Fix spelling mistakes; improve explanation spacing

--- experiments/social-follow-up/public/control/literals.js
+++ experiments/social-follow-up/public/control/literals.js
@@ -9,14 +9,18 @@ var literals = {
 
 literals.consentPage = [
 	'# Consent to Participate in Research\n',
+
 	'`CPHS# 2013-09-5632`',
 	'## Overview\n',
+
 	'This HIT is a psychology experiment being conducted by Rachit Dubey, a graduate student in Professor Tania ' +
 	'Lombrozo\'s Concepts and Cognition Lab at the University of California, Berkeley. In order to consent to ' +
 	'participate, you MUST meet the following criteria:\n',
+
 	'- 18 years of age or older.',
 	'- Fluent speaker of English.',
 	'- Have not participated in this experiment before.\n',
+
 	'This study is designed to test hypotheses about the nature of people\'s intuitive theories about the world, how ' +
 	'they can be changed, and how they influence judgments and behavior. We also hope to learn about how you ' +
 	'conceptually understand the world around you. Our intuitive sense of everyday objects and events in the world ' +
@@ -30,12 +34,15 @@ literals.consentPage = [
 	'making predictions and providing explanations), observe events, and perform actions yourself (such as grouping ' +
 	'objects). Sometimes you might watch a short video clip, look at pictures, or listen to music, and you may be ' +
 	'asked to answer questions about how the clip or music made you feel.\n',
+
 	'You may refrain from answering any questions that make you uncomfortable and may withdraw your participation at' +
 	' any time even after completing the experiment without penalty. The study takes about 10 minutes to complete,' +
 	' for which you will receive $1.25.\n',
+
 	'There are no direct benefits to you as a participant, but this research will help us understand various ' +
 	'cognitive processes, which may benefit society in a number of ways, including influencing legal and ' +
 	'public policy.\n',
+
 	'We will not be asking for any personally identifying information, and we will handle responses as ' +
 	'confidentially as possible. Worker IDs will never be tied to your responses on this survey, and IP ' +
 	'addresses will not be collected for studies that ask potentially sensitive questions. However, we cannot ' +
@@ -44,12 +51,15 @@ literals.consentPage = [
 	'and storage and will only be accessible to qualified lab personnel. We will be keeping data collected a part ' +
 	'of this experiment indefinitely. This anonymized data (containing neither Worker IDs nor IP addresses) may ' +
 	'be shared with the scientific community.\n',
+
 	'## Contact\n',
+
 	'If you have any questions about the study, feel free to contact the study investigator: Rachit Dubey at ' +
 	'`rach0012@berkeley.edu`, or the principal investigator: Tania Lombrozo at `lombrozo@berkeley.edu`. If you have ' +
 	'concerns about your rights as a participant in this experiment, please contact UC Berkeley\'s Committee for ' +
 	'Protection of Human Subjects at `(510) 642-7461` or subjects@berkeley.edu.\n',
 	'## Consent\n',
+
 	'By selecting the \'consent\' option below, I acknowledge that I am 18 or older, that I am a fluent speaker of ' +
 	'English, that I have not completed this experiment before, that I have read this consent form, and that I ' +
 	'agree to take part in the research.'
@@ -57,6 +67,7 @@ literals.consentPage = [
 
 literals.consentFailureMessage = [
 	'# Thank you!\n',
+
 	'As you do not consent to participate, please close this window and return this HIT on MTurk so that another ' +
 	'worker can accept it. If you selected this option by mistake, you can change your response and continue with ' +
 	'the experiment.\n'
@@ -64,6 +75,7 @@ literals.consentFailureMessage = [
 
 literals.instructionsOne = [
 	'# Instructions\n',
+
 	'On the following pages, you will see 10 questions that people have asked on a popular online forum. For each ' +
 	'question, we will ask you to make a series of judgments. Please press the next button below or the arrow key ' +
 	'to proceed.\n'
@@ -71,19 +83,23 @@ literals.instructionsOne = [
 
 literals.thankYouMessage = [
 	'# Thank you!\n',
+
 	'The survey is now complete, thank you for your participatation. Here is your unique SECRET key: AJFHBG897. ' +
 	'Please copy and paste this code in the box of the HIT page for the payment to process.'
 ].join('\n');
 
 literals.instructionsTwo = [
 	'# Instructions\n',
+
 	'On the next page, you will be asked select questions of the ten you have that presented with. If you could see ' +
-	'the answers for five of these questions, which five would they be?  Please select exactly *five* questions to ' +
-	'see answers for, otherwise the page will not let you submit. Please press the next button below or the arrow ' +
-	'key to proceed.\n'
+	'the answers for five of these questions, which five would they be?\n',
+
+	'Please select exactly *five* questions to see answers for, otherwise the page will not let you submit. Please ' + 
+	'press the next button below or the arrow key to proceed.\n'
 ].join('\n')
 
 literals.choosePage = [
 	'### Your responses\n',
-	'_Please remeber to select exactly **five** questions you would like to see the answers to._'
+
+	'_Please remember to select exactly **five** questions you would like to see the answers to._'
 ].join('\n');
